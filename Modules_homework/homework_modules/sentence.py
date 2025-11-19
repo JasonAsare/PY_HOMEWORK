b614690e4e036971715e2c0e527f31b341f7ceaa
Jason Asare <mypaapa.asare@gmail.com>
2025-11-19 21:39:52 +0000

    Using Modules and Functions to cr

---
+++ Modules_homework/homework_modules/sentence.py
@@ -0,0 +1,12 @@
+from homework_modules import sentence_module
+from homework_modules.sentence_module import *
+
+sen1 = sen1("Hello","Mr. Sam,")
+sen2 = sen2("it is me","Jason,")
+sen3 = sen3("Homework", "Done")
+final = final(sen1,sen2,sen3)
+
+print(sen1)
+print(sen2)
+print(sen3)
+print(final)
---
+++ Modules_homework/homework_modules/sentence_module.py
@@ -0,0 +1,11 @@
+def sen1(a,b):
+    return a+" "+b
+
+def sen2(a,b):
+    return a+" "+b
+
+def sen3(a,b):
+    return a+" "+b
+
+def final(a,b,c):
+    return a+" "+b+" "+c
